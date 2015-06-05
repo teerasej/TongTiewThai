abf3084760693e2a2593d9d91cf2802188a63727
Teerasej Jiraphatchandej <teerasej@gmail.com>
2015-06-05 15:43:54 +0700

    Add list and populate engine
    Add ListView to index.html. Update app.js to populate place list on
    startup.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,32 @@
 $(document).ready(function() {
-    
-    console.log('ready')
 
-    $.getJSON('data/place.js', function(data) {
-        console.log('data back: ' + data);
-    })
-})
+            console.log('ready');
+
+            $.mobile.loading('show');
+
+            $.getJSON('data/place.js', function(data) {
+
+                $.mobile.loading('hide');
+
+                $('#placeList').html('');
+                var item;
+                var link;
+
+                $.each(data, function(index, val) {
+                    // console.log(val.name);
+                    item = $('<li></li>');
+                    
+                    link = $('<a href="#"></a>');
+
+                    link.append('<img src="#">');
+                    link.append('<h2>Broken Bells</h2>');
+                    link.append('<p>Broken Bells</p>');
+
+                    item.append(link);
+                    $('#placeList').append(item);
+                });
+
+                // Refresh Listview
+                $('#placeList').listview('refresh');
+            });
+        });
