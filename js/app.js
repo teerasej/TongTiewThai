0510e6ae000f9351546cccd90e67cb069b534c28
Teerasej Jiraphatchandej <teerasej@gmail.com>
2015-06-06 12:04:40 +0700

    Add global variable and license

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+// Copyright (C) 2015  Teerasej Jiraphatchandej
+
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Resource URL: http://th.wikipedia.org/wiki/ไหว้พระ_9_วัด_กรุงเทพมหานคร
+
+var places;
+
 $(document).ready(function() {
 
     console.log('ready');
@@ -16,6 +35,11 @@ $(document).ready(function() {
     $.getJSON('data/place.js', function(data) {
 
         $.mobile.loading('hide');
+
+        // Keep in global variable
+        placeList = data;
+
+        //// Start populate place view
 
         $('#placeList').html('');
         var item;
